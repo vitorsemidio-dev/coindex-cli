439cdc0ceb15b3346d8aee1c8596fa7680b158b8
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-11 20:52:55 -0300

    refactor: método set
    Utilização do método setKey do keyManager

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,22 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+
+const KeyManager = require('../lib/KeyManager');
+
 const key = {
-  set() {
-    console.log('Hello from set');
+  async set() {
+    const keyManager = new KeyManager();
+    const input = await inquirer.prompt([{
+      type: 'input',
+      name: 'key',
+      message: 'Enter API Key '.green + 'https://nomics.com',
+    }]);
+
+    const key = keyManager.setKey(input.key);
+
+    if (key) {
+      console.log('API Key Set'.blue);
+    }
   },
   show() {
     console.log('Hello from show');
