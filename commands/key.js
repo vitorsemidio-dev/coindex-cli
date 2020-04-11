690de6fcdcead76b7ef29abe87aa1529dbf1031c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-11 19:43:30 -0300

    Refactor: comandos de ação

---
+++ commands/key.js
@@ -0,0 +1,13 @@
+const key = {
+  set() {
+    console.log('Hello from set');
+  },
+  show() {
+    console.log('Hello from show');
+  },
+  remove() {
+    console.log('Hello from remove');
+  },
+}
+
+module.exports = key;
